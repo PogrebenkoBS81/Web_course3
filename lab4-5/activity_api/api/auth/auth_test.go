a44b2db420bf08960a8e5053f47259b34dbc958c
PogrebenkoBS81 <pogrebenko.vasily@gmail.com>
2020-12-17 01:32:16 +0200

    Few more example tests were added

--- lab4-5/activity_api/api/auth/auth_test.go
+++ lab4-5/activity_api/api/auth/auth_test.go
@@ -52,16 +52,18 @@ func checkKey(cache cache.ICacheManager, key, value string) error {
 }
 
 // checkFetch - checks FetchAuth
-func checkFetch(t *testing.T, uuid, userID string) {
+func checkFetch(uuid, userID string) error {
 	id, err := auth.FetchAuth(uuid)
 
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 
 	if id != userID {
-		t.Fatalf("userID %s doesn't match key %s userID %s", userID, uuid, id)
+		return fmt.Errorf("userID %s doesn't match key %s userID %s", userID, uuid, id)
 	}
+
+	return nil
 }
 
 // TestService_CreateAuth - tests CreateAuth
@@ -76,7 +78,7 @@ func TestService_CreateAuth(t *testing.T) {
 	}()
 	// No need for subtests, but in 'future', if I will test another scenarios (something like border values),
 	// they could be added as another t.Run("CreateAuth_blablabla"...
-	t.Run("CreateAuth_base", func(t *testing.T) {
+	t.Run("CreateAuth_test", func(t *testing.T) {
 		userID := uuid.New().String()
 		token := getToken(userID)
 
@@ -105,7 +107,7 @@ func TestService_FetchAuth(t *testing.T) {
 		}
 	}()
 
-	t.Run("FetchAuth_test", func(t *testing.T) {
+	t.Run("FetchAuth_getExisted", func(t *testing.T) {
 		userID := uuid.New().String()
 		token := getToken(userID)
 
@@ -113,8 +115,26 @@ func TestService_FetchAuth(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		checkFetch(t, token.TokenUuid, userID)
-		checkFetch(t, token.RefreshUuid, userID)
+		if err := checkFetch(token.TokenUuid, userID); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := checkFetch(token.RefreshUuid, userID); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("FetchAuth_getInexistent", func(t *testing.T) {
+		userID := uuid.New().String()
+		token := getToken(userID)
+
+		if _, err := auth.FetchAuth(token.TokenUuid); err == nil {
+			t.Fatal("no error for invalid input")
+		}
+
+		if _, err := auth.FetchAuth(token.RefreshUuid); err == nil {
+			t.Fatal("no error for invalid input")
+		}
 	})
 }
 
@@ -129,7 +149,7 @@ func TestService_DeleteTokens(t *testing.T) {
 		}
 	}()
 
-	t.Run("DeleteTokens_test", func(t *testing.T) {
+	t.Run("DeleteTokens_DeleteReal", func(t *testing.T) {
 		userID := uuid.New().String()
 		token := getToken(userID)
 
@@ -158,6 +178,18 @@ func TestService_DeleteTokens(t *testing.T) {
 		// If key exists after deletion - throw an error
 		if err := checkKey(cacheMock, token.TokenUuid, userID); err == nil {
 			t.Fatal("token.TokenUuid exists, but should not")
+		}
+	})
+
+	t.Run("DeleteTokens_deleteInexistent", func(t *testing.T) {
+		userID := uuid.New().String()
+		token := getToken(userID)
+		// No error should be returned for deleting inexistent key
+		if err := auth.DeleteTokens(&AccessDetails{
+			TokenUuid: token.TokenUuid,
+			Username:  userID,
+		}); err != nil {
+			t.Fatal(err)
 		}
 	})
 }
@@ -198,6 +230,15 @@ func TestService_DeleteRefresh(t *testing.T) {
 		}
 		// This key shouldn't be touched
 		if err := checkKey(cacheMock, token.TokenUuid, userID); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("DeleteRefresh_deleteInexistent", func(t *testing.T) {
+		userID := uuid.New().String()
+		token := getToken(userID)
+		// No error should be returned for deleting inexistent key
+		if err := auth.DeleteRefresh(token.RefreshUuid); err != nil {
 			t.Fatal(err)
 		}
 	})
